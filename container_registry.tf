2be6595806ec917b83c9e47f34bbb661cde1bb87
danielmilewski <milewski.daniel@gmail.com>
2024-09-04 09:52:22 +0000

    kuber pipeline

--- container_registry.tf
+++ container_registry.tf
@@ -1,5 +1,35 @@
-# Create a new container registry
-resource "digitalocean_container_registry" "k8s1" {
-  name                   = "k8s1"
-  subscription_tier_slug = "starter"
+resource "kubernetes_deployment" "k8s1" {
+  metadata {
+    name = "k8s1"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "k8s1"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "k8s1"
+        }
+      }
+
+      spec {
+        container {
+          image = "registry.digitalocean.com/k8s1/k8s1:${var.docker_image_tag}"
+          name  = "k8s1"
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
 }
+
+
