58b9810413dd3a05662e629d63b072102cff05aa
danielmilewski <milewski.daniel@gmail.com>
2024-09-12 12:45:05 +0000

    added kubernetes cluster

---
+++ kubernetes_cluster.tf
@@ -0,0 +1,21 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
+  name   = "example-k8s-cluster"
+  region = "lon1"
+  version = "1.25.3-do.0"
+
+  node_pool {
+    name       = "default-pool"
+    size       = "s-2vcpu-4gb"
+    node_count = 3
+  }
+}
+
+output "kubeconfig" {
+  value = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
+  sensitive = true
+}
+
